5d2db36d93bca87621957245c25e914ddd5cdca1
Mahmudul Hasan <mahmudnotes@outlook.com>
2019-07-23 15:53:10 +0600

    Added Program

--- A-Watermelon/Program.cs
+++ A-Watermelon/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
-
-namespace A_Watermelon
+namespace CodeForces
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Hello World!");
+            var a = int.Parse(Console.ReadLine());
+            if (a > 2)
+            {
+                var c = a % 2;
+                if (c == 0)
+                {
+                    Console.WriteLine("YES");
+                }
+                else
+                {
+                    Console.WriteLine("NO");
+                }
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
         }
     }
 }
