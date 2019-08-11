19a04e54e6f2829969524f2f0e1becfc313b0887
Mahmudul Hasan <Mahmudofficialmail@gmail.com>
2019-08-11 15:19:21 +0600

    Update Program.cs

--- A - String Task/Program.cs
+++ A - String Task/Program.cs
@@ -5,21 +5,20 @@ namespace Team
     {
         public static void Main()
         {
-            int n = int.Parse(Console.ReadLine());
-            int counter = 0;
-            for (int i = 0; i < n; i++)
+            char[] userStr = Console.ReadLine().ToLower().ToCharArray();
+            string result = "";
+            foreach (var item in userStr)
             {
-                int p, v, t;
-                string[] vs = Console.ReadLine().Split();
-                p = int.Parse(vs[0]);
-                v = int.Parse(vs[1]);
-                t = int.Parse(vs[2]);
-                if (p + v + t >= 2)
+                if (item != 'a' 
+                    && item != 'e' 
+                    && item != 'i' 
+                    && item != 'o' 
+                    && item != 'u'
+                    && item != 'y')
                 {
-                    counter++;
+                    result += "." + item;
                 }
-            }
-            Console.WriteLine(counter);
+            }Console.WriteLine(result);
             Console.ReadLine();
         }
     }
